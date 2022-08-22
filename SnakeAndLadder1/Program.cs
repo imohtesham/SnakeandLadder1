8277510429889dba6450c4ebd147ada0015b5fe0
imohtesham <ibraahimm92@gmail.com>
2022-08-22 12:47:30 +0530

    Player at 0 position

---
+++ SnakeAndLadder1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SnakeAndLadder1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPosition = 0;
+            int Playeroneposition = firstPosition;
+
+            string[] Options = { "Snake", "Ladder", "rest" };
+
+
+            if (Playeroneposition < firstPosition)
+            {
+                Playeroneposition = firstPosition;
+            }
+           
+            int Player_1 = 0 ;
+
+            if (Player_1 == 0)
+            {
+                Console.WriteLine("Player_1 got: " + Options[Player_1]);
+             
+                Playeroneposition = 0;
+               
+                Console.WriteLine("Position at 0 :" + Playeroneposition + "/n");
+            }
+
+        }
+    }
+}
